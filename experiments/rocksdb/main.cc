bc682d81355b31a1abbe6768d60f1676ba13464e
imjaehyeong <dlawogud517@gmail.com>
2024-10-19 18:26:01 +0900

    fix: 락스디비 플래그 변경

--- experiments/rocksdb/main.cc
+++ experiments/rocksdb/main.cc
@@ -39,14 +39,14 @@ ABSL_FLAG(
     "The share of requests that are range queries. This value must be greater "
     "than or equal to 0.0 and less than or equal to 1.0. The share of requests "
     "that are Get requests is '1 - range_query_ratio'. (default: 0.0).");
-ABSL_FLAG(std::string, load_generator_cpus, "0",
+ABSL_FLAG(std::string, load_generator_cpus, "2",
           "The CPUs that the load generator threads run on (default: 10).");
-ABSL_FLAG(std::string, cfs_dispatcher_cpus, "1",
+ABSL_FLAG(std::string, cfs_dispatcher_cpus, "3",
           "For CFS (Linux Completely Fair Scheduler) experiments, the CPUs "
           "that the dispatchers run on (default: 11).");
-ABSL_FLAG(size_t, num_workers, 6,
+ABSL_FLAG(size_t, num_workers, 4,
           "The number of workers. Each worker has one thread. (default: 6).");
-ABSL_FLAG(std::string, worker_cpus, "2-7",
+ABSL_FLAG(std::string, worker_cpus, "4-7",
           "The CPUs that worker threads run on for CFS (Linux Completely Fair "
           "Scheduler) experiments. Each worker thread is pinned to its own "
           "CPU. Thus, the number of CPUs must be equal to the 'num_workers' "
@@ -83,7 +83,7 @@ ABSL_FLAG(absl::Duration, discard_duration, absl::Seconds(2),
           "All results from when the experiment starts to when the discard "
           "duration has elapsed are discarded. We do not want the results to "
           "include initialization costs, such as page faults. (default: 2s).");
-ABSL_FLAG(std::string, scheduler, "cfs",
+ABSL_FLAG(std::string, scheduler, "ghOSt",
           "The scheduler to use (\"cfs\" for Linux Completely Fair Scheduler "
           "or \"ghost\" for ghOSt, default: \"cfs\")");
 ABSL_FLAG(
